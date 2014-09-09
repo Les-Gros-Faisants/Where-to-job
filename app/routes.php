1f7ed68b1fdcdce0737a432f536beee77f8f8cac
Julien Ganichot <julien.ganichot@gmail.com>
2014-09-09 17:08:07 +0200

    File removal
    Removed home.blade

--- app/routes.php
+++ app/routes.php
@@ -13,17 +13,5 @@
 
 Route::get('/', function()
 {
-	return View::make('pages.home');
-});
-Route::get('about', function()
-{
-	return View::make('pages.about');
-});
-Route::get('projects', function()
-{
-	return View::make('pages.projects');
-});
-Route::get('contact', function()
-{
-	return View::make('pages.contact');
+	return View::make('home');
 });
