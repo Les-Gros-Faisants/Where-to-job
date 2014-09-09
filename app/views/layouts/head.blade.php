57b963a783ab2a35bee07b7d9f996bf2cfbb2c96
Bleeky <hausser.quentin@gmail.com>
2014-09-09 15:42:56 +0200

    Deletation Layout useless

--- app/views/layouts/head.blade.php
+++ app/views/layouts/head.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Un petit site, de style posé.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Where to Job</title>
 </head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</html>
