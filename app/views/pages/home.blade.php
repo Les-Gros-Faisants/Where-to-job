9e759dd50b5c7eb6bbf05f5cd5ee6392b0cb44e5
Julien Ganichot <julien.ganichot@gmail.com>
2014-09-09 19:28:38 +0200

    Added map + few adjustments

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.default')
 @section('content')
-Izi
+<div id='map' style='height:300px;'></div>
+<div class="ui horizontal icon divider">
+  <i class="circular search icon"></i>
+</div>
+<div class="ui fluid action input">
+  <input placeholder="Search..." type="text">
+  <div class="ui button">Search</div>
+</div>
 @stop
