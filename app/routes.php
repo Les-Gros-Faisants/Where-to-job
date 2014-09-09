a8fee90e63b56dfe3d4cb8bd33f520aa835cfe12
Mendiondo Loris <loris.mendiondo@gmail.com>
2014-09-09 10:00:38 +0200

    Base
    /home for test
    Generation of db migration files

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,8 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('home', function()
+{
+    return View::make('home');
+});
---
+++ app/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h2>Ceci est un test</h2>
+@stop
