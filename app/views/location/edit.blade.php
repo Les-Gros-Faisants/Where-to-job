5545dd8844bb8cf22fb1e9396c59468c21790b96
Mendiondo Loris <loris.mendiondo@gmail.com>
2014-09-10 11:36:37 +0200

    edit form

--- app/views/location/edit.blade.php
+++ app/views/location/edit.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.default')
 @section('content')
   EDIT LOCATION
+  {{ Form::open(array('url' => 'foo/bar')) }}
+  {{ Form::label('name', 'Name') }}
+  {{ Form::text('name', $location[0]->name) }}
+    {{ Form::close() }}
   <?php
     echo $location;
   ?>
