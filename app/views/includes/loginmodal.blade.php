5d674f13b2f3d08ae4ff31a71a1f77050fcc4c46
Bleeky <hausser.quentin@gmail.com>
2014-09-10 11:34:46 +0200

    Ceci est un commit

--- app/views/includes/loginmodal.blade.php
+++ app/views/includes/loginmodal.blade.php
@@ -1,7 +1,8 @@
 <div id="loginmodal" class="ui modal">
   <i class="close icon"></i>
   <div class="content">
-    <div class="left" style="font-family: 'Philosopher', sans-serif;">
+
+<!--     <div class="left" style="font-family: 'Philosopher', sans-serif;">
     <div style="float: left;">
       Login :
       <div class="ui input">
@@ -13,13 +14,65 @@
   <input type="text" placeholder="Search...">
 </div>
     </div>
-  </div>
-  <div class="actions">
-    <div class="ui button">
-      Cancel
+  -->
+  <div class="ui two column middle aligned relaxed grid basic segment">
+    <div class="column">
+      <div class="ui form segment">
+        <div class="field">
+          <label>Username</label>
+          <div class="ui left labeled icon input">
+            <input type="text" placeholder="Username">
+            <i class="user icon"></i>
+            <div class="ui corner label">
+              <i class="asterisk icon"></i>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <label>Password</label>
+          <div class="ui left labeled icon input">
+            <input type="password">
+            <i class="lock icon"></i>
+            <div class="ui corner label">
+              <i class="asterisk icon"></i>
+            </div>
+          </div>
+        </div>
+        <div class="ui blue submit button">Login</div>
+      </div>
     </div>
-    <div class="ui button">
-      Login
+    <div class="ui vertical divider">
+      Or
+    </div>
+    <div class="center aligned column">
+      <li style="list-style:none;margin-left:0;padding-left:0;">
+      <ul>
+          <div class="ui facebook button" style="width: 100%">
+            <i class="facebook icon"></i>
+            Facebook
+          </div>
+        </ul>
+        <ul>
+          <div class="ui twitter button" style="width: 100%">
+            <i class="twitter icon"></i>
+            Twitter
+          </div>
+        </ul>
+        <ul>
+          <div class="ui google plus button" style="width: 100%">
+            <i class="google plus icon"></i>
+            Google Plus
+          </div>          
+        </ul>
+        <ul>
+          <div class="medium green ui icon button" style="width: 100%">
+            <i class="signup icon"></i>
+            Sign Up
+          </div>          
+        </ul>
+      </li>
     </div>
   </div>
+
+</div>
 </div>
