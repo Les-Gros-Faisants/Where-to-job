8869d561c854f4234fe45744a5ab62a1da98113d
Mendiondo Loris <loris.mendiondo@gmail.com>
2014-09-10 16:17:17 +0200

    form lcoation edit

--- app/views/location/edit.blade.php
+++ app/views/location/edit.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('content')
-  EDIT LOCATION
+EDIT LOCATION
+{{ Form::open(array('url' => 'users')) }}
 
+{{ Form::label('name', 'Name') }}
+{{ Form::text('name', null, array('placeholder'=>$location[0]->name)) }}
+
+{{ Form::label('city', 'City') }}
+{{ Form::text('city', null, array('placeholder'=>$location[0]->city)) }}
+
+
+{{ Form::submit('Click Me!') }}
+
+{{ Form::close() }}
 @stop
