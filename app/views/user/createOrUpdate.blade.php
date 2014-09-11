5fb96d18d815669ae05cd541844846d361f5ad80
Bleeky <hausser.quentin@gmail.com>
2014-09-11 18:09:11 +0200

    REPARE

--- app/views/user/createOrUpdate.blade.php
+++ app/views/user/createOrUpdate.blade.php
@@ -24,48 +24,85 @@ $columns = Schema::getColumnListing('users');
 @endif
 @endforeach
 
+
 <div class="ui error form segment">
 	<div class="three fields">
 		<div class="field">
 			{{ Form::label('First Name', 'First Name') }}
-			{{ Form::text('First Name', null, array('placeholder'=>$user[0]->'First Name')) }}
+			@if (isset($user))
+			{{ Form::text('First Name', null, array('placeholder'=>$user[0]->firstname)) }}
+			@else
+			{{ Form::text('First Name', null, array('placeholder'=>'First Name')) }}
+			@endif
 		</div>
 		<div class="field">
 			{{ Form::label('Last Name', 'Last Name') }}
-			{{ Form::text('Last Name', null, array('placeholder'=>$user[0]->'Last Name')) }}
+			@if (isset($user))
+			{{ Form::text('Last Name', null, array('placeholder'=>$user[0]->lastname)) }}
+			@else
+			{{ Form::text('Last Name', null, array('placeholder'=>'Last Name')) }}
+			@endif
 		</div>
-		<div class="field">
+<!-- 		<div class="field">
 			{{ Form::label('Username', 'Username') }}
-			{{ Form::text('Username', null, array('placeholder'=>$user[0]->'Username')) }}
-		</div>
+			@if (isset($user))
+			{{ Form::text('Username', null, array('placeholder'=>$user[0]->username)) }}
+@else
+			{{ Form::text('Username', null, array('placeholder'=>'Username')) }}
+			@endif
+		</div> -->
 	</div>
 	<div class="two fields">
 		<div class="field">
 			{{ Form::label('Password', 'Password') }}
-			{{ Form::password('Password', null, array('placeholder'=>$user[0]->'Password')) }}
+			@if (isset($user))
+			{{ Form::password('Password', null, array('placeholder'=>$user[0]->password)) }}
+			@else
+			{{ Form::password('Password', null, array('placeholder'=>'Password')) }}
+			@endif
 		</div>
 		<div class="field">
 			{{ Form::label('Email', 'Email') }}
-			{{ Form::email('Email', $value = null, $attributes = array('placeholder'=>$user[0]->'Email')) }}
+			@if (isset($user))
+			{{ Form::email('Email', $value = null, $attributes = array('placeholder'=>$user[0]->email)) }}
+			@else
+			{{ Form::email('Email', $value = null, $attributes = array('placeholder'=>'Email')) }}
+			@endif
 		</div>
 	</div>
 	<div class="three fields">
 		<div class="field">
-			{{ Form::label('Adress', 'Adress') }}
-			{{ Form::text('Adress', null, array('placeholder'=>$user[0]->'Adress')) }}
+			{{ Form::label('Addres', 'Addres') }}
+			@if (isset($user))
+			{{ Form::text('Addres', null, array('placeholder'=>$user[0]->address)) }}
+			@else
+			{{ Form::text('Addres', null, array('placeholder'=>'Addres')) }}
+			@endif
 		</div>
 		<div class="field">
 			{{ Form::label('City', 'City') }}
-			{{ Form::text('City', null, array('placeholder'=>$user[0]->'City')) }}
+			@if (isset($user))
+			{{ Form::text('City', null, array('placeholder'=>$user[0]->city)) }}
+			@else
+			{{ Form::text('City', null, array('placeholder'=>'City')) }}
+			@endif
 		</div>
 		<div class="field">
 			{{ Form::label('Job', 'Job') }}
-			{{ Form::text('Job', null, array('placeholder'=>$user[0]->'Job')) }}
+			@if (isset($user))
+			{{ Form::text('Job', null, array('placeholder'=>$user[0]->job)) }}
+			@else
+			{{ Form::text('Job', null, array('placeholder'=>'Job')) }}
+			@endif
 		</div>
 	</div>
 	<div class="field">
 		{{ Form::label('Infos', 'Infos') }}
-		{{ Form::textarea('Infos', null, array('placeholder'=>$user[0]->'Infos')) }}
+		@if (isset($user))
+		{{ Form::textarea('Infos', null, array('placeholder'=>$user[0]->infos)) }}
+		@else
+		{{ Form::textarea('Infos', null, array('placeholder'=>'Infos')) }}
+		@endif
 	</div>
 	<div class="field">
 		{{ Form::label('Photo', 'Photo') }}
