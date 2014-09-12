527822495fdff2a3fe856bfaba9489e23a5dcd24
Bleeky <hausser.quentin@gmail.com>
2014-09-12 12:52:56 +0200

    Dynamic Sidebar

--- app/views/includes/connected.blade.php
+++ app/views/includes/connected.blade.php
@@ -1,20 +1,16 @@
-<!-- @extends('includes.sidebar')
 
-	@section('connected') -->
-	<a id="logoutbutton" class="item">
-		<div style="vertical-align: center;">
-			<i class="sign out icon"></i>
-		</div>
-	</a>
-	<a class="item" href="/">
-		<div style="vertical-align: center;">
-			<i class="add icon"></i>
-		</div>
-	</a>
-	<a class="item">
-		<div style="vertical-align: center;">
-			<i class="setting icon"></i>
-		</div>
-	</a>
-
-<!-- @stop -->
+<a id="logoutbutton" class="item">
+	<div style="vertical-align: center;">
+		<i class="sign out icon"></i>
+	</div>
+</a>
+<a class="item" href="/">
+	<div style="vertical-align: center;">
+		<i class="add icon"></i>
+	</div>
+</a>
+<a class="item">
+	<div style="vertical-align: center;">
+		<i class="setting icon"></i>
+	</div>
+</a>
