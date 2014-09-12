57fc796a8a42e182acaf1e99883a2bf49a4e29d6
Arkeopix <mendiej750@gmail.com>
2014-09-12 10:35:33 +0200

    kicked geolocation control

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -9,9 +9,9 @@ function custom_find_me( map ) {
 
     custom_fm.id = 'fm_control';
     custom_fm.style.position = 'absolute';
-    custom_fm.style.zIndex = '50';
-    custom_fm.style.width = '20px';
-    custom_fm.style.height = '42px';
+    custom_fm.style.zIndex = '50px';
+    custom_fm.style.width = '25px';
+    custom_fm.style.height = '25px';
     custom_fm.style.top = '5px';
     custom_fm.style.right = '2px';
     custom_fm.style.backgroundImage = "url('../assets/images/waving_man_sat.png')";
@@ -68,18 +68,13 @@ function load_map( string ) {
 	    map.addShape(user);
 	});
 	
-	MQA.withModule( 'smallzoom', 'geolocationcontrol', 'mousewheel', function() {
+	MQA.withModule( 'smallzoom', 'mousewheel', function() {
 	    
 	    map.addControl(
 		new MQA.SmallZoom(),
 		new MQA.MapCornerPlacement( MQA.MapCorner.TOP_LEFT, new MQA.Size( 5,5 ) )
 	    );
 	    
-	    // map.addControl(
-	    // 	new MQA.GeolocationControl(),
-	    // 	new MQA.MapCornerPlacement( MQA.MapCorner.TOP_RIGHT, new MQA.Size( 10,10 ) )
-	    // );
-
 	    custom_find_me( map );
 	    
 	    map.enableMouseWheelZoom();
