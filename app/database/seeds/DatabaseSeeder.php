d72402be1ac08006a63cff738e60bc155f3334bd
Bleeky <hausser.quentin@gmail.com>
2014-09-13 13:43:21 +0200

    Here you seed.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,9 +12,8 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('UserTableSeeder');
-		$this->command->info('LOL PD !');
+		$this->command->info("CA C'EST DU SEED BATARD.");
 	}
-
 }
 
 class UserTableSeeder extends Seeder {
@@ -23,11 +22,9 @@ class UserTableSeeder extends Seeder {
 
 		DB::table('comments')->delete();
 		DB::table('users')->delete();
-		DB::table('migrations')->delete();
 		DB::table('locations')->delete();
 
-
-		User::create(array(
+		$mainuser = User::create(array(
 			'email'         => 'cul@mail.com',
 			'lastname'         => 'cul',
 			'firstname'         => 'cul',
@@ -37,32 +34,52 @@ class UserTableSeeder extends Seeder {
 			'job'         => 'culiste',
 			'infos'         => 'groscul',
 			));
-
 		$this->command->info('User created : mail : cul@mail.com : password : cul');
 
-		// Location::create(array(
-		// 	'location'  => 'rue des gros culs',
-		// 	'city'  => 'grosculcity',
-		// 	'name'  => 'le bar des culs',
-		// 	'user_id' => '2',
-		// 	'ambiance' => 'culisee'
-		// 	));
-		// Location::create(array(
-		// 	'location'  => 'rue des petits culs',
-		// 	'city'  => 'petitculcity',
-		// 	'name'  => 'le bar des petits culs',
-		// 	'user_id' => '2',
-		// 	'ambiance' => 'nulle'
-		// 	));
-		// Location::create(array(
-		// 	'location'  => 'rue des mega culs',
-		// 	'city'  => 'megaculcity',
-		// 	'name'  => 'le bar des mega culs',
-		// 	'user_id' => '2',
-		// 	'ambiance' => 'defonce'
-		// 	));
+		$mainuser2 = User::create(array(
+			'email'         => 'bite@mail.com',
+			'lastname'         => 'bite',
+			'firstname'         => 'bite',
+			'password'         => Hash::make('bite'),
+			'address'         => 'rue du bite',
+			'city'         => 'bitecity',
+			'job'         => 'biteiste',
+			'infos'         => 'grosbite',
+			));
 
-		// $this->command->info('Locations crées');	
+		$this->command->info('User created : mail : bite@mail.com : password : bite');
+
+		$location1 = Location::create(array(
+			'location'  => 'rue des gros culs',
+			'city'  => 'grosculcity',
+			'name'  => 'le bar des culs',
+			'ambiance' => 'culisee',
+			'user_id' => $mainuser->id
+			));
+		$location2 = Location::create(array(
+			'location'  => 'rue blah',
+			'city'  => 'Strasbourg',
+			'name'  => 'Exile',
+			'ambiance' => 'Sobre',
+			'user_id' => $mainuser->id
+			));
+		$location3 = Location::create(array(
+			'location'  => 'rue bloh',
+			'city'  => 'Nancy',
+			'name'  => 'PD',
+			'ambiance' => 'merdique',
+			'user_id' => $mainuser->id
+			));
+		$location4 = Location::create(array(
+			'location'  => 'rue des Juifs',
+			'city'  => 'Jerusalem',
+			'name'  => 'Kipa',
+			'ambiance' => 'juive',
+			'user_id' => $mainuser2->id
+			));
+
+
+		$this->command->info('Locations crees');
 
 	}
 
