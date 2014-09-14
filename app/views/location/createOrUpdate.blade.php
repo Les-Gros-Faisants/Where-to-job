b34d4793f5a027a870287d327a01f0b5853ac113
Mendiondo Loris <loris.mendiondo@gmail.com>
2014-09-14 16:47:38 +0200

    Location

--- app/views/location/createOrUpdate.blade.php
+++ app/views/location/createOrUpdate.blade.php
@@ -4,7 +4,8 @@
 // get all columns from location table
 $columns = Schema::getColumnListing('locations');
 ?>
-@if (Auth::check())
+<div style="max-width: 60%; margin: auto;">
+    @if (Auth::check())
 @if (isset($location))
 EDIT LOCATION
 {{ Form::model($location, array('route' => array('location.update', $location[0]->id))) }}
@@ -12,50 +13,50 @@ EDIT LOCATION
 CREATE LOCATION
 {{ Form::open(array('route' => 'location.store')) }}
 @endif
-<div class="ui error form segment">
-    <div class="two fields">
-        <div class="field">
-            {{ Form::label('Location', 'Location') }}
+    <div class="ui error form segment">
+        <div class="two fields">
+            <div class="field">
+                {{ Form::label('Location', 'Location') }}
 			@if (isset($location))
 			{{ Form::text('location', null, array('placeholder'=>$location[0]->location)) }}
 			@else
 			{{ Form::text('location', null, array('placeholder'=>'Location')) }}
 			@endif
-        </div>
-        <div class="field">
-            {{ Form::label('City', 'City') }}
+            </div>
+            <div class="field">
+                {{ Form::label('City', 'City') }}
 			@if (isset($location))
 			{{ Form::text('city', null, array('placeholder'=>$location[0]->city)) }}
 			@else
 			{{ Form::text('city', null, array('placeholder'=>'City')) }}
 			@endif
+            </div>
         </div>
-    </div>
-    <div class="field">
-        {{ Form::label('Name', 'Na') }}
+        <div class="field">
+            {{ Form::label('Name', 'Na') }}
 			@if (isset($location))
 			{{ Form::text('name', null, array('placeholder'=>$location[0]->name)) }}
 			@else
 			{{ Form::text('name', null, array('placeholder'=>'Name')) }}
 			@endif
-    </div>
-    <div class="two fields">
-        <div class="field">
-            {{ Form::label('Ambiance', 'Ambiance') }}
+        </div>
+        <div class="two fields">
+            <div class="field">
+                {{ Form::label('Ambiance', 'Ambiance') }}
 			@if (isset($location))
 			{{ Form::text('ambiance', null, array('placeholder'=>$location[0]->ambiance)) }}
 			@else
 			{{ Form::text('ambiance', null, array('placeholder'=>'Ambiance')) }}
 			@endif
+            </div>
         </div>
-    </div>
-    <div class="field">
-        {{ Form::label('Photo', 'Photo') }}
+        <div class="field">
+            {{ Form::label('Photo', 'Photo') }}
 		{{ Form::file('photos', $attributes = array()) }}
+        </div>
+        {{ Form::submit('Submit', array('class'=>'ui blue submit button')) }}
     </div>
-    {{ Form::submit('Submit', array('class'=>'ui blue submit button')) }}
 </div>
-
 {{ Form::close() }}
 @else
 Please log in
