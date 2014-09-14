ce6e7c4dd9592d5d08cd0ac4471416874ac6ca09
Julien Ganichot <julien.ganichot@gmail.com>
2014-09-14 14:32:25 +0200

    Few ui adjustments

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -9,9 +9,10 @@ function doPost()
     {
       if (data.length < 1)
         {
-          $("#results").html("<div id=\"no_result\" class=\"ui segment\" style=\"padding: auto;margin: auto;text-align: center;\"> \
+          $("#results").html("<div id=\"no_result\" class=\"ui segment\" style=\"padding: auto;margin: auto;text-align: center;display: none;\"> \
                                       <h2>No results found for \"" + $("#city_field").val() + "\"</h2> \
                                      </div>");
+          $("#no_result").fadeIn( "fast", function(){});
           return (false);
         }
       else
